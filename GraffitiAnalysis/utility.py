549090f756351b870d3810167223885a95a5ea3c
Greg Thomsen <gthomsen@gmail.com>
2016-08-08 22:52:15 -0500

    Paths: utility.py Topic: Utility Module Exif Orientation Flag Conversion Routine Support Update Description:   * Added a routine that converts an Exif orientation flag into the     corresponding number of degrees an associated image was rotated to     position it.  This is useful for rotating an image for display to match     the photographer's intended orientation.

--- GraffitiAnalysis/utility.py
+++ GraffitiAnalysis/utility.py
@@ -36,3 +36,48 @@ def datetime_string_to_timestamp( string, date_separator="/", time_separator=":"
         seconds = 0
 
     return seconds
+
+def exif_orientation_to_rotation( orientation ):
+    """
+    Converts the Exif orientation value into the corresponding number of
+    degrees rotated, clockwise, the camera was when the image was captured.
+    Unknown Exif orientations will cause a RuntimeError exception to be
+    raised.
+
+    NOTE: This is not the number of degrees needed to correct for the
+          Exif orientation flag.
+
+    NOTE: Not all Exif orientations are supported since they cannot be
+          expressed as a simple rotation - some require a rotation and
+          a flip about an axis.  Currently only the following orientations
+          are supported:
+
+            1, 3, 6, and 8
+
+    Takes 1 argument:
+
+      orientation - Exif orientation value.  See the Exif standards for
+                    the full range of values and above for the values
+                    supported.
+
+    Returns 1 value:
+
+      degrees - Degrees, clockwise, the camera was rotated when the image
+                was captured.
+
+    """
+
+    if orientation == 1:
+        # no rotation needed.
+        return 0
+    elif orientation == 3:
+        # image is upside down.
+        return 180
+    elif orientation == 6:
+        # image is rotated counter-clockwise.
+        return 270
+    elif orientation == 8:
+        # image is rotated clockwise.
+        return 90
+
+    raise RuntimeError( "Unknown Exif orientation seen {:d}!".format( orientation ) )
